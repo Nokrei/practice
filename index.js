01cd7155a1c55bfce178b3fb6330e120848b609d
unknown <dive877@gmail.com>
2020-04-07 17:45:05 +0400

    Add media query

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ window.onscroll = function() {
   let currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("navbar").style.top = "0";
-    document.getElementById("toTop").style.opacity = "0";
+    document.getElementById("toTop").style.visibility = "hidden";
   } else {
     document.getElementById("navbar").style.top = "-150px";
-    document.getElementById("toTop").style.opacity = "1";
+    document.getElementById("toTop").style.visibility = "visible";
   }
   prevScrollpos = currentScrollPos;
 }
